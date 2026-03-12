9565616a82d896ddb91f883b8fd93b847e2e6137
annklin <frnksulivan@gmail.com>
2026-03-12 14:19:26 -0300

    first commit

---
+++ android/app/src/main/java/app/lovable/aa1646eb938b483d941bdc95db20b728/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.aa1646eb938b483d941bdc95db20b728;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react'
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/nutri-buddy-coach-10/",
-  server: {
+    server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
